618fd5e796b50fca44d9d5e315213a69f4028837
amos-kibet <kibetamos97@gmail.com>
2022-05-02 05:03:46 +0300

    Edit '/' endpoint response
    Change from 'Welcome to African Reactors Backend' to 'Welcome to Auth App Backend'

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ db.mongoose
 
 // simple route
 app.get("/", (req, res) => {
-  res.json({ message: "Welcome to Africa Reactors Backend." });
+  res.json({ message: "Welcome to Auth App Backend." });
 });
 
 // routes
